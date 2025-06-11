aebc4738203238442cb0aea211d537182d544dc9
Roman Yaschenkov <ryaschenkov@yandex.ru>
2025-06-11 14:20:33 +0300

    initial commit
    Initial commit for new practicum project ImageFeed.

---
+++ ImageFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageFeed
+//
+//  Created by Roman Yaschenkov on 11.06.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
