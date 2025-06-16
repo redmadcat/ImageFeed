44becb9aa10482aeaaf772a1a6018e33a585e20a
Roman Yaschenkov <ryaschenkov@yandex.ru>
2025-06-16 12:01:31 +0300

    add: outlet connections
    Add outlet connections to ImageListCell.

--- ImageFeed/ImageList/ImagesListCell.swift
+++ ImageFeed/ImageList/ImagesListCell.swift
@@ -8,5 +8,11 @@
 import UIKit
 
 final class ImagesListCell: UITableViewCell {
+    // MARK: - @IBOutlet
+    @IBOutlet weak var cellImage: UIImageView!
+    @IBOutlet weak var likeButton: UIButton!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    // MARK: - Definition
     static let reuseIdentifier = "ImagesListCell"
 }
