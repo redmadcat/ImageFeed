d4a964aec3a3f68494b718eb3230dda17a1828eb
Roman Yaschenkov <ryaschenkov@yandex.ru>
2025-06-17 16:29:06 +0300

    add: single image view/view controller
    Add SingleImageView/ViewController.

---
+++ ImageFeed/Presentation/ImageList/SingleImageViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by Roman Yaschenkov on 17.06.2025.
+//
+
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+    @IBOutlet weak var imageView: UIImageView!
+    
+}
