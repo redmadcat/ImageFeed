36ced3e051642247630cb720ea03c387a84b32db
Roman Yaschenkov <ryaschenkov@yandex.ru>
2025-06-20 11:35:38 +0300

    add: share button impl
    Add share button implementation.

--- ImageFeed/Presentation/ImageList/SingleImageViewController.swift
+++ ImageFeed/Presentation/ImageList/SingleImageViewController.swift
@@ -8,37 +8,37 @@
 import UIKit
 
 final class SingleImageViewController: UIViewController, UIScrollViewDelegate {
+    // MARK: - @IBOutlet
     @IBOutlet weak private var imageView: UIImageView!
     @IBOutlet weak private var scrollView: UIScrollView!
     
+    // MARK: - Definition
     var image: UIImage? {
         didSet {
-            guard isViewLoaded else { return }
-            
-            if let image {
-                imageView.image = image
-                rescaleAndCenterImageInScrollView(image: image)
-            }
+            guard isViewLoaded, let image else { return }
+            imageView.image = image
+            imageView.frame.size = image.size
+            rescaleAndCenterImageInScrollView(image: image)
         }
     }
     
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         scrollView.minimumZoomScale = 0.1
         scrollView.maximumZoomScale = 1.25
         
         guard let image else { return }
-        
         imageView.image = image
         imageView.frame.size = image.size
         rescaleAndCenterImageInScrollView(image: image)
     }
     
+    // MARK: - Private functions
     private func rescaleAndCenterImageInScrollView(image: UIImage) {
         let minZoomScale = scrollView.minimumZoomScale
         let maxZoomScale = scrollView.maximumZoomScale
-        scrollView.layoutIfNeeded()
+        view.layoutIfNeeded()
         let visibleRectSize = scrollView.bounds.size
         let imageSize = image.size
         let hScale = visibleRectSize.width / imageSize.width
@@ -47,8 +47,8 @@ final class SingleImageViewController: UIViewController, UIScrollViewDelegate {
         scrollView.setZoomScale(scale, animated: false)
         scrollView.layoutIfNeeded()
         let newContentSize = scrollView.contentSize
-        let x = (newContentSize.width - visibleRectSize.width)
-        let y = (newContentSize.height - visibleRectSize.height)
+        let x = (newContentSize.width - visibleRectSize.width) / 2
+        let y = (newContentSize.height - visibleRectSize.height) / 2
         scrollView.setContentOffset(CGPoint(x: x, y: y), animated: false)
     }
     
@@ -57,7 +57,30 @@ final class SingleImageViewController: UIViewController, UIScrollViewDelegate {
         return imageView
     }
     
+    // MARK: - @IBAction
     @IBAction private func didTapBackButton() {
         dismiss(animated: true, completion: nil)
     }
+    
+    @IBAction private func didTapShareButton() {
+        guard let image else { return }
+        let share = UIActivityViewController(activityItems: [image], applicationActivities: nil)
+        present(share, animated: true, completion: nil)
+    }
 }
+
+#if DEBUG
+import SwiftUI
+
+@available(iOS 13, *)
+struct SingleImageViewControllerPreview: PreviewProvider {
+    static var previews: some View {
+        ForEach(UIViewController.devices, id: \.self) { deviceName in
+            UIStoryboard(name: "Main", bundle: nil).instantiateViewController(identifier: "SingleImage").toPreview()
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+                .previewDisplayName(deviceName)
+        }
+    }
+}
+#endif
+
--- ImageFeed/Presentation/Profile/ProfileViewController.swift
+++ ImageFeed/Presentation/Profile/ProfileViewController.swift
@@ -8,12 +8,14 @@
 import UIKit
 
 final class ProfileViewController: UIViewController {
+    // MARK: - @IBOutlet
     @IBOutlet weak private var avatarImageView: UIImageView!
     @IBOutlet weak private var nameLabel: UILabel!
     @IBOutlet weak private var loginNameLabel: UILabel!
     @IBOutlet weak private var descriptionLabel: UILabel!
     @IBOutlet weak private var logoutButton: UIButton!
         
+    // MARK: - @IBAction
     @IBAction func didTapLogoutButton() {
     }
 }
