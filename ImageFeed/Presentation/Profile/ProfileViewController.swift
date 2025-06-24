ed3887bc307eddec99d222d8dc4a630e84de41f5
Roman Yaschenkov <ryaschenkov@yandex.ru>
2025-06-24 16:12:32 +0300

    refactor: use code based profile layout
    Refactor, use profile layout implementation from code.

--- ImageFeed/Presentation/Profile/ProfileViewController.swift
+++ ImageFeed/Presentation/Profile/ProfileViewController.swift
@@ -8,15 +8,66 @@
 import UIKit
 
 final class ProfileViewController: UIViewController {
-    // MARK: - @IBOutlet
-    @IBOutlet weak private var avatarImageView: UIImageView!
-    @IBOutlet weak private var nameLabel: UILabel!
-    @IBOutlet weak private var loginNameLabel: UILabel!
-    @IBOutlet weak private var descriptionLabel: UILabel!
-    @IBOutlet weak private var logoutButton: UIButton!
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureLayout()
+    }
+    
+    // MARK: - Private functions
+    private func configureLayout() {
+        let logoutButton = UIButton.systemButton(
+            with: UIImage(named: "Logout") ?? UIImage(),
+            target: self,
+            action: #selector(didTapLogoutButton))
+        logoutButton.translatesAutoresizingMaskIntoConstraints = false
+        logoutButton.tintColor = .ypRed
+                
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.image = UIImage(named: "Avatar")
+        imageView.tintColor = .ypGray
         
+        let nameLabel = UILabel(
+            text: "Екатерина Новикова",
+            textColor: UIColor.ypWhite,
+            font:.systemFont(ofSize: 23, weight: .semibold))
+        let loginNameLabel = UILabel(text: "@ekaterina_nov", textColor: UIColor.ypGray)
+        let descriptionLabel = UILabel(text: "Hello, world!", textColor: UIColor.ypWhite)
+        
+        view.addSubview(imageView)
+        view.addSubview(logoutButton)
+        view.addSubview(nameLabel)
+        view.addSubview(loginNameLabel)
+        view.addSubview(descriptionLabel)
+        
+        NSLayoutConstraint.activate([
+            imageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 32),
+            imageView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            imageView.widthAnchor.constraint(equalToConstant: 70),
+            imageView.heightAnchor.constraint(equalTo: imageView.widthAnchor, multiplier: 1.0 / 1.0),
+            
+            logoutButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16),
+            logoutButton.centerYAnchor.constraint(equalTo: imageView.centerYAnchor),
+            logoutButton.widthAnchor.constraint(equalToConstant: 44),
+            logoutButton.heightAnchor.constraint(equalToConstant: 44),
+                        
+            nameLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 8),
+            nameLabel.leadingAnchor.constraint(equalTo: imageView.leadingAnchor),
+            nameLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16),
+            
+            loginNameLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 8),
+            loginNameLabel.leadingAnchor.constraint(equalTo: nameLabel.leadingAnchor),
+            loginNameLabel.trailingAnchor.constraint(equalTo: nameLabel.trailingAnchor),
+            
+            descriptionLabel.topAnchor.constraint(equalTo: loginNameLabel.bottomAnchor, constant: 8),
+            descriptionLabel.leadingAnchor.constraint(equalTo: nameLabel.leadingAnchor),
+            descriptionLabel.trailingAnchor.constraint(equalTo: nameLabel.trailingAnchor)
+        ])
+    }
+    
     // MARK: - @IBAction
-    @IBAction func didTapLogoutButton() {
+    @objc private func didTapLogoutButton() {
     }
 }
 
