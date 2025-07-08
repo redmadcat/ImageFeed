d240c83fc83eb08cb40009a04fa687264cc33ee7
Roman Yaschenkov <ryaschenkov@yandex.ru>
2025-07-08 10:32:54 +0300

    add: oauth service impl
    Add OAuth2Sevice implementation.

---
+++ ImageFeed/Services/OAuth2Service.swift
@@ -0,0 +1,56 @@
+//
+//  OAuth2Service.swift
+//  ImageFeed
+//
+//  Created by Roman Yaschenkov on 04.07.2025.
+//
+
+import UIKit
+
+final class OAuth2Service {
+    static let shared = OAuth2Service()
+    
+    private init() { }
+    
+    func fetchOAuthToken(code: String, completion: @escaping (Result<String, Error>) -> Void) {
+        guard let request = makeOAuthTokenRequest(code: code) else { return }
+                
+        let task = URLSession.shared.data(for: request) { result in
+            switch result {
+            case .success(let data):
+                do {
+                    let response = try JSONDecoder().decode(OAuthTokenResponseBody.self, from: data)
+                    completion(.success(response.accessToken))
+                } catch {
+                    completion(.failure(error))
+                }
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+        
+        task.resume()
+    }
+    
+    private func makeOAuthTokenRequest(code: String) -> URLRequest? {
+        guard var urlComponents = URLComponents(string: "https://unsplash.com/oauth/token") else {
+            return nil
+        }
+        
+        urlComponents.queryItems = [
+            URLQueryItem(name: "client_id", value: Constants.accessKey),
+            URLQueryItem(name: "client_secret", value: Constants.secretKey),
+            URLQueryItem(name: "redirect_uri", value: Constants.redirectURI),
+            URLQueryItem(name: "code", value: code),
+            URLQueryItem(name: "grant_type", value: "authorization_code")
+        ]
+        
+        guard let authTokenUrl = urlComponents.url else {
+            return nil
+        }
+        
+        var request = URLRequest(url: authTokenUrl)
+        request.httpMethod = "POST"
+        return request
+    }
+}
