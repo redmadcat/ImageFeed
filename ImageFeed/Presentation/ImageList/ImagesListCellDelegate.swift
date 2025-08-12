f9d6b61a8f865b657c3606a8ca699555b0d5c07e
Roman Yaschenkov <ryaschenkov@yandex.ru>
2025-08-12 14:14:06 +0300

    add: images list cell delegate decl
    Add ImagesListCellDelegate declaration.

---
+++ ImageFeed/Presentation/ImageList/ImagesListCellDelegate.swift
@@ -0,0 +1,10 @@
+//
+//  ImagesListCellDelegate.swift
+//  ImageFeed
+//
+//  Created by Roman Yaschenkov on 12.08.2025.
+//
+
+protocol ImagesListCellDelegate: AnyObject {
+    func imageListCellDidTapLike(_ cell: ImagesListCell)
+}
