3d825d56569f266bf0246d6c5d6ee2aace51f601
Roman Yaschenkov <ryaschenkov@yandex.ru>
2025-08-12 10:35:47 +0300

    add: load image from network
    Add load image from network based on imagesListService+Kingfisher dependency.

--- ImageFeed/Presentation/ImageList/ImagesListViewController.swift
+++ ImageFeed/Presentation/ImageList/ImagesListViewController.swift
@@ -6,19 +6,29 @@
 //
 
 import UIKit
+import Kingfisher
 
 final class ImagesListViewController: UIViewController {
     // MARK: - @IBOutlet
     @IBOutlet weak private var tableView: UITableView!
     
     // MARK: - Definition
-    let photoNames = (0..<20).map { String($0) }
+    var photos: [Photo] = []
+    let imagesListService = ImagesListService.shared
     
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableView.contentInset = UIEdgeInsets(top: 10, left:0, bottom: 12, right: 0)
+        imagesListService.fetchPhotosNextPage()
+        
+        NotificationCenter.default.addObserver(
+            forName: ImagesListService.didChangeNotification,
+            object: nil,
+            queue: .main) { _ in
+                self.updateTableViewAnimated()
+            }
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -29,22 +39,62 @@ final class ImagesListViewController: UIViewController {
             else {
                 fatalError("Invalid segue destination!")
             }
-            viewController.image = UIImage(named: photoNames[indexPath.row])
+//            viewController.image = UIImage(named: photoNames[indexPath.row])
+//            viewController.image?.kf. =  self.photos[safe: indexPath.row]
         } else {
             super.prepare(for: segue, sender: sender)
         }
     }
 
     func configCell(for cell: ImagesListCell, with indexPath: IndexPath) {
-        guard let image = UIImage(named: photoNames[indexPath.row]) else { return }
-        let likeImage = UIImage(named: indexPath.row % 2 == 0 ? "FavoritesActive" : "FavoritesNoActive")
-        cell.cellImage.image = image
-        cell.dateLabel.text = Date().longDateString
+        let photo = photos[indexPath.row]
+        
+        let placeholderImage = UIImage(systemName: "person.circle.fill")?
+            .withTintColor(.lightGray, renderingMode: .alwaysOriginal)
+            .withConfiguration(UIImage.SymbolConfiguration(pointSize: 70, weight: .regular, scale: .large))
+        
+        cell.cellImage.kf.indicatorType = .activity
+        cell.cellImage.kf.setImage(
+            with: URL(string: photo.thumbImageURL),
+            placeholder: placeholderImage,
+            options: [
+                .scaleFactor(UIScreen.main.scale),
+                .cacheOriginalImage,
+                .forceRefresh
+            ]) { result in
+                switch result {
+                case .success(let value):
+                    print(value.image)
+                    print(value.cacheType)
+                    print(value.source)
+                case .failure(let error):
+                    log(error.localizedDescription)
+                }
+            }
+        
+        let likeImage = UIImage(named: photo.isLiked ? "FavoritesActive" : "FavoritesNoActive")
+        cell.dateLabel.text = photo.createdAt?.longDateString
         cell.likeButton.setImage(likeImage, for: .normal)
     }
     
     func getSingleImageSegueIdentifier() -> String {
         "ShowSingleImage"
+    }
+    
+    // MARK: - Private func
+    private func updateTableViewAnimated() {
+        let oldCount = photos.count
+        let newCount = imagesListService.photos.count
+        photos = imagesListService.photos
+        
+        if oldCount != newCount {
+            tableView.performBatchUpdates {
+                let indexPaths = (oldCount..<newCount).map { i in
+                    IndexPath(row: i, section: 0)
+                }
+                tableView.insertRows(at: indexPaths, with: .automatic)
+            } completion: { _ in }
+        }
     }
 }
 
