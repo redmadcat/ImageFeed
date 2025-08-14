85e4e616ac980b7fb528323b1e5c5800e8bc08d4
Roman Yaschenkov <ryaschenkov@yandex.ru>
2025-08-14 13:29:10 +0300

    fix: remove photo result unnecessary members
    Fix, remove unnecessary members from photo result struct.

--- ImageFeed/Services/ImageList/ImagesListService.swift
+++ ImageFeed/Services/ImageList/ImagesListService.swift
@@ -10,12 +10,8 @@ import Foundation
 private struct PhotoResult: Codable {
     let id: String
     let createdAt: String
-    let updatedAt: String
     let width: Int
     let height: Int
-    let color: String
-    let blurHash: String
-    let likes: Int
     let likedByUser: Bool
     let description: String?
     let urls: UrlsResult
