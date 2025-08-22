c83f8e9ac439f2011ee598ee914122b8e0a6e86e
Roman Yaschenkov <ryaschenkov@yandex.ru>
2025-08-22 13:13:31 +0300

    add: stub test members
    Add stub members for unit test purpose.

---
+++ ImageFeedTests/WebViewPresenterSpy.swift
@@ -0,0 +1,26 @@
+//
+//  WebViewPresenterSpy.swift
+//  ImageFeed
+//
+//  Created by Roman Yaschenkov on 22.08.2025.
+//
+
+import ImageFeed
+import Foundation
+
+final class WebViewPresenterSpy: WebViewPresenterProtocol {
+    var viewDidLoadCalled: Bool = false
+    var view: WebViewViewControllerProtocol?
+    
+    func viewDidLoad() {
+        viewDidLoadCalled = true
+    }
+    
+    func didUpdateProgressValue(_ newValue: Double) {
+        
+    }
+    
+    func code(from url: URL) -> String? {
+        return nil
+    }
+}
---
+++ ImageFeedTests/WebViewViewControllerSpy.swift
@@ -0,0 +1,24 @@
+//
+//  WebViewViewControllerSpy.swift
+//  ImageFeed
+//
+//  Created by Roman Yaschenkov on 22.08.2025.
+//
+
+import ImageFeed
+import Foundation
+
+final class WebViewViewControllerSpy: WebViewViewControllerProtocol {
+    var presenter: WebViewPresenterProtocol?
+    var loadRequestCalled: Bool = false
+    
+    func load(request: URLRequest) {
+        loadRequestCalled = true
+    }
+    
+    func setProgressValue(_ newValue: Float) {
+    }
+    
+    func setProgressHidden(_ isHidden: Bool) {
+    }
+}
