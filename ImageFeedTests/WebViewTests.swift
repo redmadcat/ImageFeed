6d05593425167c96d9fa9eac6f81481a68fce1db
Roman Yaschenkov <ryaschenkov@yandex.ru>
2025-08-25 13:05:34 +0300

    add: web view unit tests
    Add web view unit tests.

--- ImageFeedTests/WebViewTests.swift
+++ ImageFeedTests/WebViewTests.swift
@@ -10,27 +10,106 @@ import XCTest
 
 final class WebViewTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testViewControllerCallsViewDidLoad() {
+        // given
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let viewController = storyboard.instantiateViewController(withIdentifier: "WebViewViewController") as! WebViewViewController
+        let presenter = WebViewPresenterSpy()
+        viewController.presenter = presenter
+        presenter.view = viewController
+        
+        // when
+        _ = viewController.view
+        
+        // then
+        XCTAssertTrue(presenter.viewDidLoadCalled)
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    func testPresenterCallsLoadRequest() {
+        // given
+        let viewController = WebViewViewControllerSpy()
+        let authHelper = AuthHelper()
+        let presenter = WebViewPresenter(authHelper: authHelper)
+        viewController.presenter = presenter
+        presenter.view = viewController
+        
+        // when
+        presenter.viewDidLoad()
+        
+        // then
+        XCTAssertTrue(viewController.loadRequestCalled)
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    func testProgressVisibleWhenLessThenOne() {
+        // ginev
+        let authHelper = AuthHelper()
+        let presenter = WebViewPresenter(authHelper: authHelper)
+        let progress: Float = 0.6
+        
+        // when
+        let shouldHideProgress = presenter.shouldHideProgress(for: progress)
+        
+        // then
+        XCTAssertFalse(shouldHideProgress)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
+    
+    func testProgressHiddenWhenOne() {
+        // ginev
+        let authHelper = AuthHelper()
+        let presenter = WebViewPresenter(authHelper: authHelper)
+        let progress: Float = 1.0
+        
+        // when
+        let shouldHideProgress = presenter.shouldHideProgress(for: progress)
+        
+        // then
+        XCTAssertTrue(shouldHideProgress)
+    }
+    
+    func testAuthHelperAuthURL() {
+        // given
+        let configuration = AuthConfiguration.standard
+        let authHelper = AuthHelper(configuration: configuration)
+        
+        // when
+        let url = authHelper.authURL()
+        
+        guard let urlString = url?.absoluteString else {
+            XCTFail("Auth URL is nil")
+            return
         }
+        
+        // then
+        XCTAssertTrue(urlString.contains(configuration.authURLString))
+        XCTAssertTrue(urlString.contains(configuration.accessKey))
+        XCTAssertTrue(urlString.contains(configuration.redirectURI))
+        XCTAssertTrue(urlString.contains("code"))
+        XCTAssertTrue(urlString.contains(configuration.accessScope))
     }
-
+    
+    func testCodeFromURL() {
+        // given
+        let testCode = "test code"
+        guard var urlComponents = URLComponents(string: "https://unsplash.com/oauth/authorize/native") else {
+            XCTFail("URL is nil")
+            return
+        }
+        
+        urlComponents.queryItems = [
+            URLQueryItem(name: "code", value: testCode)
+        ]
+        
+        let authHelper = AuthHelper()
+        
+        guard let url = urlComponents.url else {
+            XCTFail("URL is nil")
+            return
+        }
+        
+        // when
+        let code = authHelper.code(from: url)
+        
+        // then
+        XCTAssertEqual(code, testCode)
+    }
 }
