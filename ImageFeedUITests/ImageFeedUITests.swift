f3ead5f056dc0c383cc91043fdfb95a46de27b0d
Roman Yaschenkov <ryaschenkov@yandex.ru>
2025-08-31 16:49:19 +0300

    add: images list ui tests members
    Add ImagesList UI tests members.

---
+++ ImageFeedUITests/ImageFeedUITests.swift
@@ -0,0 +1,30 @@
+//
+//  ImageFeedUITests.swift
+//  ImageFeedUITests
+//
+//  Created by Roman Yaschenkov on 31.08.2025.
+//
+
+import XCTest
+
+final class ImageFeedUITests: XCTestCase {
+    private let app = XCUIApplication()
+    
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+
+        app.launch()
+    }
+    
+    func testAuth() throws {
+        
+    }
+    
+    func testFeed() throws {
+        
+    }
+    
+    func testProfile() throws {
+        
+    }
+}
