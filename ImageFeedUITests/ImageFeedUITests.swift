c89cce0dfaa0f4e42fa6e9b669bf3525623c3de7
Roman Yaschenkov <ryaschenkov@yandex.ru>
2025-09-01 12:16:47 +0300

    add: imagefeed ui tests impl
    Add ImageFeed UITests implementation.

--- ImageFeedUITests/ImageFeedUITests.swift
+++ ImageFeedUITests/ImageFeedUITests.swift
@@ -17,14 +17,72 @@ final class ImageFeedUITests: XCTestCase {
     }
     
     func testAuth() throws {
+        app.buttons["Authenticate"].tap()
         
+        let webView = app.webViews["UnsplashWebView"]
+        
+        XCTAssertTrue(webView.waitForExistence(timeout: 5))
+        
+        let loginTextField = webView.descendants(matching: .textField).element
+        XCTAssertTrue(loginTextField.waitForExistence(timeout: 5))
+        
+        loginTextField.tap()
+        loginTextField.typeText("")
+        webView.swipeUp()
+        
+        let passwordTextField = webView.descendants(matching: .secureTextField).element
+        XCTAssertTrue(passwordTextField.waitForExistence(timeout: 5))
+        
+        passwordTextField.tap()
+        passwordTextField.typeText("")
+        webView.swipeUp()
+        
+        webView.buttons["Login"].tap()
+        
+        let tablesQuery = app.tables
+        let cell = tablesQuery.children(matching: .cell).element(boundBy: 0)
+        
+        XCTAssertTrue(cell.waitForExistence(timeout: 5))
     }
-    
-    func testFeed() throws {
         
+    func testFeed() throws {
+        let tablesQuery = app.tables
+        
+        let cell = tablesQuery.children(matching: .cell).element(boundBy: 0)
+        cell.swipeUp()
+        
+        sleep(2)
+        
+        let cellToLike = tablesQuery.children(matching: .cell).element(boundBy: 1)
+        
+        cellToLike.buttons["Like"].tap()
+        cellToLike.buttons["Like"].isAccessibilityElement = true
+        sleep(2)
+        cellToLike.buttons["Like"].tap()
+        
+        sleep(2)
+        
+        cellToLike.tap()
+        
+        sleep(2)
+        
+        let image = app.scrollViews.images.element(boundBy: 0)
+        
+        image.pinch(withScale: 3, velocity: 1)
+        image.pinch(withScale: 0.5, velocity: -1)
+        
+        let navigationBackButton = app.buttons["NavigationBack"]
+        navigationBackButton.tap()
     }
     
     func testProfile() throws {
+        sleep(3)
+        app.tabBars.buttons.element(boundBy: 1).tap()
         
+        XCTAssertTrue(app.staticTexts["Name Lastname"].exists)
+        XCTAssertTrue(app.staticTexts["@username"].exists)
+        
+        app.buttons["Logout"].tap()
+        app.alerts["Пока, пока!"].scrollViews.otherElements.buttons["Да"].tap()
     }
 }
