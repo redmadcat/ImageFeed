a805d1ef615d7e1f72409a29a64777646dff19f3
Roman Yaschenkov <ryaschenkov@yandex.ru>
2025-09-02 20:23:40 +0300

    refactor: images list view presenter proto impl
    Refactor, ImagesListViewPresenterProtocol implementation, remove deprecated members.

--- ImageFeed/Presentation/ImageList/ImagesListViewPresenterProtocol.swift
+++ ImageFeed/Presentation/ImageList/ImagesListViewPresenterProtocol.swift
@@ -10,10 +10,13 @@ import Foundation
 public protocol ImagesListViewPresenterProtocol: DisposableProtocol {
     var view: ImagesListViewControllerProtocol? { get set }
     func didLoad()
-    func fetchPhotosNextPage()
     func photosCount() -> Int
-    func imageSizeAt(indexPath: IndexPath) -> CGSize?
-    func largeImageURLAt(indexPath: IndexPath) -> String
-    func imageDetailsAt(indexPath: IndexPath) -> (thumbImageURL: String, createdAt: Date?, isLiked: Bool)?
+    func willDisplayAt(indexPath: IndexPath)
     func changeLikeAt(indexPath: IndexPath) -> Bool?
+    func photoInfoAt(indexPath: IndexPath) -> (
+        largeImage: String,
+        thumbImage: String,
+        createdAt: Date?,
+        isLiked: Bool,
+        size: CGSize)?
 }
